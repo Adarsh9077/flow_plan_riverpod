cdb1e1e810c0300ce42dd74fd9b86e9ff395093c
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-06-27 14:27:08 +0530

    task_modal

---
+++ lib/common/models/user_modal.dart
@@ -0,0 +1,17 @@
+// import 'dart:convert';
+//
+// UserModal userModalFromJson(String str) => UserModal.fromJson(json);
+//
+// String userModalToJson(UserModal data) => json.encode(data.toJson());
+
+class UserModal {
+  final int id;
+  final int isVerified;
+
+  UserModal({required this.id, required this.isVerified});
+
+  factory UserModal.fromJson(Map<String, dynamic> json) =>
+      UserModal(id: json["id"], isVerified: json["isVerified"]);
+
+  Map<String, dynamic> toJson() => {"id": id, "isVerified": isVerified};
+}
