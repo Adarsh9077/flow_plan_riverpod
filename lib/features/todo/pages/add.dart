f3837320250650f7f851828a751210800627f3d8
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-07-04 23:59:41 +0530

    Working on Add task

--- lib/features/todo/pages/add.dart
+++ lib/features/todo/pages/add.dart
@@ -1,4 +1,9 @@
+import 'package:flow_plan/common/utils/constants.dart';
+import 'package:flow_plan/common/widgets/app_style.dart';
+import 'package:flow_plan/common/widgets/custom_text.dart';
+import 'package:flow_plan/common/widgets/height_spacer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AddTask extends ConsumerStatefulWidget {
@@ -11,6 +16,21 @@ class AddTask extends ConsumerStatefulWidget {
 class _AddTaskState extends ConsumerState<AddTask> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: ListView(
+          children: [
+            HeightSpacer(height: 30),
+            CustomTextField(
+              controller: controller,
+              hintText: "Add Title",
+              hintStyle: appStyle(22, AppConst.kGreyLight, FontWeight.w600),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 } // 06:23:00
