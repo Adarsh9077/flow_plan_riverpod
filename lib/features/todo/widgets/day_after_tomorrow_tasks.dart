26004c608ce573ecccec3cef584ec3cc0765283a
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-07-10 15:19:19 +0530

    work on day After tomorrow task

--- lib/features/todo/widgets/day_after_tomorrow_tasks.dart
+++ lib/features/todo/widgets/day_after_tomorrow_tasks.dart
@@ -1,6 +1,9 @@
+import 'package:flow_plan/common/utils/constants.dart';
 import 'package:flow_plan/common/widgets/expansion_tile_custom.dart';
+import 'package:flow_plan/features/todo/controllers/expansion_provider.dart';
 import 'package:flow_plan/features/todo/controllers/todo/todo_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class DayAfterTomorrowTasks extends ConsumerWidget {
@@ -18,6 +21,15 @@ class DayAfterTomorrowTasks extends ConsumerWidget {
     return ExpansionTileCustom(
       title: dayAfterTomorrow.substring(5),
       subTitle: "Day After Tomorrow Tasks",
+      onExpansionChanged: (bool expanded) {
+        ref.read(expansionState1Provider.notifier).setStart(!expanded);
+      },
+      trailing: Padding(
+        padding: EdgeInsets.only(right: 12),
+        child: ref.watch(expansionState1Provider)
+            ? Icon(AntDesign.circledown, color: AppConst.kLight)
+            : Icon(AntDesign.closecircleo, color: AppConst.kBlueLight),
+      ),
       children: [],
     );
   }
