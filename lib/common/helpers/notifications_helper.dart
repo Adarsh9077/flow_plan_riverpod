6244c9f70dc9561fd2bc578502aa9f8a0cf236da
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-09-17 10:36:51 +0530

    working on Notification

--- lib/common/helpers/notifications_helper.dart
+++ lib/common/helpers/notifications_helper.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
 class NotificationsHelper {
   final WidgetRef ref;
@@ -13,5 +16,63 @@ class NotificationsHelper {
   final BehaviorSubject<String?> selectNotificationSubject =
       BehaviorSubject<String?>();
 
+  initializeNotification() async {
+    _configureSelectNotificationSubject();
+    await _configureLocalTimeZone();
+    final DarwinInitializationSettings initializationSettingsIos =
+        DarwinInitializationSettings(
+          requestAlertPermission: false,
+          requestSoundPermission: false,
+          requestBadgePermission: false,
+          onDidReceiveLocalNotification: onDidReceiveLocalNotification,
+        );
+    final AndroidInitializationSettings androidInitializationSettings =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+          iOS: initializationSettingsIos,
+          android: androidInitializationSettings,
+        );
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (data) async {
+        if (data != null) {
+          debugPrint("notification payload: ${data.payload}");
+        }
+        selectNotificationSubject.add(data.payload);
+      },
+    );
+  }
 
-} // 10:48:45
+  void requestIosPermissions() async {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+          IOSFlutterLocalNotificationsPlugin
+        >()
+        ?.requestPermissions(alert: true, badge: true, sound: true);
+  }
+
+  Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    const String timeZoneName = 'Asia/Kolkata';
+    tz.setLocalLocation(tz.getLocation(timeZoneName));
+  }
+
+  Future onDidReceiveLocalNotification(
+    int id,
+    String? title,
+    String? body,
+    String? payload,
+  ) async {
+    showDialog(
+      context: ref.context,
+      builder: (context) => CupertinoAlertDialog(
+        title: Text(title ?? ""),
+        content: Text(body ?? ""),
+        actions: [CupertinoDialogAction(
+          isDestructiveAction: true,
+            child: Text("Close"))],
+      ),
+    );
+  }
+} // 11:05:00
